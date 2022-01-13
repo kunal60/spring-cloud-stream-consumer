362460ec338a8bb9092e4f73aed31a5f450a9d99
Kunal Malhotra <kunal.malhotra@idemia.com>
2022-01-13 18:57:28 +0530

    Working consumer

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring-cloud-stream-publisher
+Read from spring=cloud-stream=producer github readme file
+
+For help: https://www.youtube.com/watch?v=1IgvFlyEPkY&list=PLVz2XdJiJQxwoGuQb8lR-sTV26dz1SoXo&index=4
+
+          
--- src/main/java/com/kunal/spring/cloud/stream/api/SpringCloudStreamConsumerApplication.java
+++ src/main/java/com/kunal/spring/cloud/stream/api/SpringCloudStreamConsumerApplication.java
@@ -1,13 +1,26 @@
 package com.kunal.spring.cloud.stream.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class) // In producer we added source.class. I consumer we added Sinc.class
 public class SpringCloudStreamConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudStreamConsumerApplication.class, args);
-	}
+    private Logger logger = LoggerFactory.getLogger(SpringCloudStreamConsumerApplication.class);
 
+    @StreamListener("input") // input is the channel name
+    public void consumeMessage(Book book) {
+        logger.info("Consume payload : " + book);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamConsumerApplication.class, args);
+    }
 }
